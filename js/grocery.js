b8f618dff3d856b27544cc30e74b47f9aec8d41a
Virginia Ortuño Moreno <virginiaortunomoreno@gmail.com>
2021-10-21 20:25:06 +0200

    Refactor

--- js/grocery.js
+++ js/grocery.js
@@ -74,16 +74,29 @@ var subtotal = {
 };
 var total = 0;
 
-// Exercise 1
-function addToCartList(id) {
+// Exercise 1-5-7
+function addToCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    cart = [];
     for(let i=0; i<products.length; i++) {
         let product = products[i];
         if (product.id === id) {
             cartList.push(product);
         }
     }
+    for (let i=0; i<cartList.length; i++) {
+        let product = cartList[i];
+        if (cart.indexOf(product) === -1) {
+            product.quantity = 1;
+            product.subtotal = product.price * product.quantity;
+            cart.push(product);
+        } else {
+            product.quantity++;
+            product.subtotal = product.price * product.quantity;
+        }
+    }
+    calculateSubtotals();
 }
 
 // Exercise 2
@@ -99,50 +112,31 @@ function calculateSubtotals() {
     subtotal.grocery.value = 0;
     subtotal.beauty.value = 0;
     subtotal.clothes.value = 0;
-    for(let i=0; i<cartList.length; i++) {
-        let product = cartList[i];
+    for(let i=0; i<cart.length; i++) {
+        let product = cart[i];
         switch(product.type){
             case 'grocery':
-                subtotal.grocery.value += product.price;
+                subtotal.grocery.value += product.price * product.quantity;
                 break;
             case 'beauty':
-                subtotal.beauty.value += product.price;
+                subtotal.beauty.value += product.price * product.quantity;
                 break;
             case 'clothes':
-                subtotal.clothes.value += product.price;
+                subtotal.clothes.value += product.price * product.quantity;
                 break;
         }
     }
+    calculateTotal();
 }
 
 // Exercise 4
 function calculateTotal() {
-    // Calculate total price of the cart either using the "cartList" array
+    // Calculate total price of the cart
     total = 0;
-    for (let product in cartList){
-        total += cartList[product].price;
+    for (let sub in subtotal){
+        total += subtotal[sub].value;
     }
-}
-
-// Exercise -
-function applyPromotionsSubtotals() {
-
-}
-
-// Exercise 5
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    cart = [];
-    for (let i=0; i<cartList.length; i++) {
-        let product = cartList[i];
-        if (cart.indexOf(product) === -1) {
-            product.quantity = 1;
-            cart.push(product);
-        } else {
-            product.quantity++;
-        }
-    }
+    applyPromotionsCart();
 }
 
 // Exercise 6
@@ -163,26 +157,13 @@ function applyPromotionsCart() {
         } 
 }
 
-// Exercise 7
-function addToCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
-
 // Exercise 9
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 }
 
-
-
 // Exercise 10
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
-}
-
-function comprar() {
-    generateCart();
-    applyPromotionsCart();
 }
